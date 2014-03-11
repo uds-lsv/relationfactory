64c58ee8711c5a5be484cd7e110feeb228ea9187
beroth <rothbenj@gmail.com>
2014-03-11 21:54:26 +0100

    building java components

---
+++ components/pipeline/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd $TAC_ROOT/components/pipeline
+#JAVA_HOME=$TAC_ROOT/lib/java/jdk1.6.0_18/
+LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:./lib/antlib/*
+ANT_HOME=./lib/antlib
+$JAVA_HOME/bin/java -Dant.home=$ANT_HOME -classpath $LOCALCLASSPATH org.apache.tools.ant.Main $*
