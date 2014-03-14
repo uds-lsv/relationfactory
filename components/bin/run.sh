8de7d59112f8254cce807c000479fd7441973475
Benjamin Roth <beroth@blake.cs.umass.edu>
2014-03-14 15:52:05 -0400

    run script uses system wide jre

--- components/bin/run.sh
+++ components/bin/run.sh
@@ -2,5 +2,5 @@
 LOG4J=file://$TAC_ROOT/config/log4j.config
 
 COMP=$TAC_ROOT/components/pipeline/
-JAVA_HOME=$TAC_ROOT/lib/java/jdk1.6.0_18/
-$JAVA_HOME/bin/java -Dfile.encoding=UTF8 -Dlog4j.configuration=$LOG4J -cp $COMP/dist/components.jar:$COMP/lib/* -Xmx32g $@
+#JAVA_HOME=$TAC_ROOT/lib/java/jdk1.6.0_18/
+java -Dfile.encoding=UTF8 -Dlog4j.configuration=$LOG4J -cp $COMP/dist/components.jar:$COMP/lib/* -Xmx32g $@
