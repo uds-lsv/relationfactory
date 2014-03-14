bd697a2a3dd1e0a3f33d161c9a76317536e5c5f1
beroth <rothbenj@gmail.com>
2014-03-14 16:32:31 +0100

    updated setup scripts

--- bin/generate_system.sh
+++ bin/generate_system.sh
@@ -2,35 +2,18 @@
 
 $TAC_ROOT/components/pipeline/build.sh
 
-#echo
-#echo "compiling sentence segmentation module"
-#cd $TAC_ROOT/components/sentence_segmentation/
-#make
-#echo "compiling ner module"
-#cd $TAC_ROOT/components/ner/
-#make
-
-
-#echo
-#if [ -d "$TAC_ROOT/lib/ghc/bin" ]; then
-#echo "haskell installed"
-#else
-#echo "setting up haskell"
-#  $TAC_ROOT/lib/ghc/build.sh
-#fi
-
 echo "compiling ner module"
 cd $TAC_ROOT/components/ner/
 make
 
-
-
-#echo
-#echo "compiling classifier"
-#cd $TAC_ROOT/lib/svm_light
-#tar xzf svm_light.tar.gz
-#make
-#cd -
+echo
+echo "compiling classifier"
+mkdir -p $TAC_ROOT/lib/svm_light
+cd $TAC_ROOT/lib/svm_light
+wget http://download.joachims.org/svm_light/current/svm_light.tar.gz
+tar xzf svm_light.tar.gz
+make
+cd -
 
 
 
